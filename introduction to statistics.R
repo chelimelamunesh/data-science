ac902d878237e700fdc843959795342f464585aa
chelimelamunesh <chelimelamunesh@gmail.com>
2024-09-24 02:03:52 +0530

    Add files via upload

---
+++ introduction to statistics.R
@@ -0,0 +1,64 @@
+5+2
+7-5
+7*8
+7/5
+7+2-1/4
+(7-2)/(7/8)
+#commands
+(3^3)-(2^8)+50+500
+exp(5)
+log(4)
+pi
+1*2*3*4*5
+sqrt(5)
+36<65
+price <- 52
+price
+class(price)
+munesh = 98
+class(munesh)
+seq(1,1000)
+1:5
+rep(1,50)
+rep(5,5)
+rep(munesh,5)
+rep("chinna",6)
+x <- c(5,1,4,5)
+y <- c(4,5,6,8)
+x+y
+x+4
+sqrt(x)
+exp(2)
+exp(1)
+log(4,base = exp(2))
+x = c(1,8,9,3)
+y = c(4,8,3,6)
+df = data.frame(x,y)
+df
+a = c(1,2,3,4)
+b = c("munesh","vamshi","tharun","nikhil")
+df1 = data.frame(a,b)
+df1
+a[2:4]
+a[2] = 24
+a[2]
+a[2] <- 24
+a
+df[4,1]
+df[3,2]
+df[2, ]
+df[ ,1]
+df[c(1,3,4),2]
+df[c(2,2), c(2, 2)]
+df[c(1,2), c(1, 2)]
+df$x
+df$y
+View(df)
+data()
+cars
+BJsales
+USArrests
+mtcars
+ToothGrowth
+help(mtcars)
+help(cars)
