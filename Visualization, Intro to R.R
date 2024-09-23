96a2c752799a1a6521729a39f63234979c35b590
chelimelamunesh <chelimelamunesh@gmail.com>
2024-09-24 02:10:14 +0530

    Add files via upload

---
+++ Visualization, Intro to R.R
@@ -0,0 +1,38 @@
+seq(1,50,100)
+seq(1,100)
+seq_len(length.out = munesh)
+seq1 <- seq(1,50)
+seq2 <- seq(80,100)
+combined_seq <- c(seq1,seq2)
+combined_seq
+marks <-c(1,23,7,8,9)
+marks
+marks[1]
+marks[c(1,3)]
+marks[-3]
+marks[3*5]
+marks[2*3]
+marks[c(2*3)]
+marks[1+3]
+marks[1*3]
+marks[1/3]
+marks[1-23]
+marks< 9
+marks>3
+marks[1-3]
+marks[1-4]
+marks[c(1-3)]
+marks[1]-marks[2]
+marks[marks>60] <- 8888  
+marks
+marks[1:3]
+marks[2:4]
+#data frame 
+x <- c(62,8,53,45,75,69)
+y <- c("A","B","C","D","E","F")
+df <-data.frame(x,y)
+df
+a = c(1,5,8,9,10)
+b = c("M","U","N","E","S")
+df1=data.frame(a,b)
+df1
